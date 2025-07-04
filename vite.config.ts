8a2092eaa7fc52f0d77d6c2a34f0daf1ef380b42
anntnzrb <anntnzrb@proton.me>
2025-07-04 10:10:47 -0500

    chore: add dependencies and update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [
 		TanStackRouterVite({ autoCodeSplitting: true }),
 		viteReact(),
 		tailwindcss(),
+		tsconfigPaths(),
 	],
 });
